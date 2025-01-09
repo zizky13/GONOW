a3b05875b43cfabe0c7902d1e2dfddcd5a8f277b
zizky13 <zikar.nurizky@gmail.com>
2025-01-09 10:13:42 +0700

    added map point selector

--- components/LocationBasedModal.tsx
+++ components/LocationBasedModal.tsx
@@ -1,23 +1,56 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Button, RadioButton, TextInput } from "react-native-paper";
-import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import { mazarine, pensive } from "../constants/Colors";
-import {
-    addDocumentToLocBased,
-    addDocumentToTimeBased,
-} from "@/utils/firestore";
+import { addDocumentToLocBased } from "@/utils/firestore";
 import { useModalState } from "@/hooks/modalState";
 import { GeoPoint } from "firebase/firestore";
+import { Modal } from "react-native";
+import * as Mapbox from '@rnmapbox/maps';
+import * as Location from 'expo-location';
+
+
+Mapbox.setAccessToken(
+    process.env.EXPO_PUBLIC_MAPBOX_KEY ? process.env.EXPO_PUBLIC_MAPBOX_KEY : "",
+);
 
 const LocationBasedModal = () => {
+    const mapRef = React.useRef(null);
     const [checked, setChecked] = React.useState("nonPrio");
     const [title, setTitle] = React.useState("");
     const [description, setDescription] = React.useState("");
+    const [mapVisible, setMapVisible] = React.useState(false);
+    const [currentLocation, setCurrentLocation] = React.useState({
+        latitude: -6.291815,
+        longitude: 106.726430,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
     const [lat, setLat] = React.useState(0.0);
     const [long, setLong] = React.useState(0.0);
     const [radius, setRadius] = React.useState(0);
     const { hideModal } = useModalState();
+
+    const getCurrentLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+            alert("Permission denied");
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setCurrentLocation({
+            ...currentLocation,
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+        });
+        setLat(location.coords.latitude);
+        setLong(location.coords.longitude);
+    };
+
+    React.useEffect(() => {
+        getCurrentLocation();
+    }, []);
 
     return (
         <View style={styles.formContainer}>
@@ -31,20 +64,6 @@ const LocationBasedModal = () => {
                 label="Deskripsi"
                 style={{ margin: 8 }}
                 onChangeText={(text) => setDescription(text)}
-            />
-
-            <TextInput
-                keyboardType="number-pad"
-                label="Latitude"
-                style={{ margin: 8 }}
-                onChangeText={(num) => setLat(parseFloat(num))}
-            />
-
-            <TextInput
-                keyboardType="number-pad"
-                label="Longitude"
-                style={{ margin: 8 }}
-                onChangeText={(num) => setLong(parseFloat(num))}
             />
 
             <TextInput
@@ -90,8 +109,55 @@ const LocationBasedModal = () => {
                 </View>
             </View>
 
-            <View style={styles.pickerView}>
-            </View>
+            <Button
+                mode="outlined"
+                style={{ margin: 8 }}
+                onPress={() => setMapVisible(true)}
+            >
+                Pick Location on Map
+            </Button>
+
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={mapVisible}
+                onRequestClose={() => setMapVisible(false)}
+            >
+                <View style={styles.mapContainer}>
+                    <Mapbox.MapView
+                        ref={mapRef}
+                        style={styles.map}
+                        compassEnabled={true}
+                        logoEnabled={false}
+                        scaleBarEnabled={false}
+                    >
+                        <Mapbox.Camera
+                            defaultSettings={{
+                                centerCoordinate: [long, lat],
+                                zoomLevel: 14
+                            }}
+                        />
+                        <Mapbox.PointAnnotation
+                            id="selectedLocation"
+                            coordinate={[long, lat]}
+                            draggable
+                            onDragEnd={(e) => {
+                                setLat(e.geometry.coordinates[1]);
+                                setLong(e.geometry.coordinates[0]);
+                            }}
+                        >
+                            <View />
+                        </Mapbox.PointAnnotation>
+                    </Mapbox.MapView>
+                    <Button
+                        mode="contained"
+                        onPress={() => setMapVisible(false)}
+                        style={styles.confirmButton}
+                    >
+                        Confirm Location
+                    </Button>
+                </View>
+            </Modal>
 
             <Button
                 mode="contained"
@@ -153,5 +219,19 @@ const styles = StyleSheet.create({
     radioContainer: {
         flexDirection: "row",
         margin: 8,
+    },
+
+    mapContainer: {
+        flex: 1,
+        position: "relative",
+    },
+    map: {
+        flex: 1,
+    },
+    confirmButton: {
+        position: "absolute",
+        bottom: 20,
+        alignSelf: "center",
+        width: "90%",
     },
 });
